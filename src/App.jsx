26599c08c456ee7296a0504bcf58922411d9353c
Jitesh Singh <jiteshs101@gmail.com>
2024-09-13 17:23:20 +0530

    Get The IP Address Of Visitor(api.ipify.org) & Sent to the Backend(ipwhois.io) Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css'
-import VideoUpload from './components/VideoUpload'
 import { Toaster } from 'react-hot-toast';
+import VideoUpload from './components/VideoUpload'
 import HomeFeed from './components/HomeFeed';
 
 
 function App() {
+
+  const [ipAddress, setIpAddress] = useState('');
+
+  useEffect(() => {
+    // Function to fetch IP address
+    const fetchIpAddress = async () => {
+      try {
+        // Fetch IP from an external service ipify
+        const response = await axios.get('https://api.ipify.org?format=json');
+        const ip = response.data.ip;
+
+        setIpAddress(ip);
+
+        // Send IP address to the backend
+        await sendIpToBackend(ip);
+      } catch (error) {
+        console.error('Error fetching IP address : ', error);
+      }
+    };
+
+    fetchIpAddress();
+  }, []);
+
+  // Function to send IP address to the backend
+  const sendIpToBackend = async (ip) => {
+    try {
+      const response = await axios.get(`https://streamflix.koyeb.app/api/v1/visitor/save/${ip}`);
+      console.log('IP address sent to backend : ', response.data);
+    } catch (error) {
+      console.error('Error sending IP to backend : ', error);
+    }
+  };
+
   return (
     <>
       <div className='flex justify-center py-8 bg-indigo-400'>
@@ -18,7 +53,7 @@ function App() {
         <Toaster />
         <VideoUpload />
       </div>
-      
+
       <div className="flex flex-col items-center justify-center space-y-2 py-6">
         <HomeFeed />
       </div>
